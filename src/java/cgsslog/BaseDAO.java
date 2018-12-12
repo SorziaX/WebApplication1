2c72522b4b8a77b9639baf74caca4f91ba067ff7
Dsakuraff <dsakuraff@163.com>
2018-12-12 13:54:23 +0900

    add property file

--- src/java/cgsslog/BaseDAO.java
+++ src/java/cgsslog/BaseDAO.java
@@ -5,9 +5,18 @@
  */
 package cgsslog;
 import com.mysql.jdbc.Driver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -27,21 +36,37 @@ public class BaseDAO {
         this.closeDB();
     }
     
-    public void connectDB(){
+    public void connectDB() {
         try{
+            System.out.println("DEBUG:connectDB Start");
+            Properties prop = new Properties();
+            InputStream in = null;
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            in = classLoader.getResourceAsStream("mysql.properties");
+            System.out.println("DEBUG:4");
+            prop.load(in);
+            System.out.println("DEBUG:5");
+            
             //数据库信息
-            String userName="root"; 
+            String mysqlServer=prop.getProperty("mysqlServer");
+            //用户名
+            String userName=prop.getProperty("userName"); 
             //密码 
-            String userPasswd="mysql_shen_pass"; 
+            String userPasswd=prop.getProperty("userPasswd"); 
             //数据库名 
-            String dbName="mycgss_log"; 
+            String dbName=prop.getProperty("dbName"); 
 
-            String url="jdbc:mysql://localhost/"+dbName+"?user="+userName+"&password="+userPasswd;
+            String url="jdbc:mysql://"+mysqlServer+"/"+dbName+"?user="+userName+"&password="+userPasswd;
+            System.out.println(url);
 
             this.con = DriverManager.getConnection(url);
             
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
     
